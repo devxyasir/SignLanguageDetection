7f6af57a1753e52a95e4e29ceda962c920fda19a
Muhammad Yasir <jamyasir0534@gmail.com>
2025-02-13 23:15:01 +0500

    setup developed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='signLanguage',
+    version='0.0.0',
+    author='devxyasir',
+    author_email='jamyasir0534@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
